211bd280499d47781e8e668a96eaf90c1f9873fa
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-03-12 18:56:30 +0900

    トップページに取得したpostsテーブルのレコードを表示 (#3)

---
+++ frontend/src/components/PostItem.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let title: string;
+  export let author: string;
+</script>
+
+<li class="border p-2">
+  <h1 class="font-bold">{title}</h1>
+  <p>by {author}</p>
+</li>
+
+<style lang="scss">
+
+</style>
