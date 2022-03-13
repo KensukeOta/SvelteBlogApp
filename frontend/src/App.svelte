45d290c678ef44d3779f8018ad09411be99a8078
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-03-13 10:59:48 +0900

    Bootstrap IconsをCDN経由でダウンロードし、記事更新ページに遷移するリンクボタンを実装 (#7)

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { Router, Route } from "svelte-routing";
 	import BlogPost from "./pages/BlogPost.svelte";
+	import EditForm from "./pages/EditForm.svelte";
 	import Home from "./pages/Home.svelte";
 	import LoginForm from "./pages/LoginForm.svelte";
 	import PostForm from "./pages/PostForm.svelte";
@@ -15,6 +16,9 @@
 	<Route path="/posts/create" component="{PostForm}" />
 	<Route path="/posts/:id" let:params>
 		<BlogPost id="{params.id}" />
+	</Route>
+	<Route path="/posts/:id/edit" let:params>
+		<EditForm id="{params.id}" />
 	</Route>
 	<Route path="/"><Home /></Route>
 </Router>
