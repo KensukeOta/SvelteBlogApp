3ab0358ff843c5fca6414be4d6b987542ad9bbb9
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-03-13 09:21:59 +0900

    記事詳細ページを作成し、記事の内容を表示 (#5)

--- frontend/src/components/PostItem.svelte
+++ frontend/src/components/PostItem.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+  import AnchorButton from "./atom/AnchorButton.svelte";
+
   export let post;
 </script>
 
 <li class="border p-2">
-  <h1 class="font-bold">{post.title}</h1>
+  <h1 class="font-bold"><AnchorButton path={`/posts/${post.id}`}>{post.title}</AnchorButton></h1>
   <p>by {post.user.name}</p>
 </li>
 
